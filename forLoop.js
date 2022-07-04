b16a8e24ef2988085fad2e8c988215a22217e97f
devcww <chimiwangchukw@gmail.com>
2022-07-04 13:47:02 +0600

    feat/loop

--- forLoop.js
+++ forLoop.js
@@ -70,3 +70,105 @@ while (guess !== target) {
 }
 console.log(`Target: ${target} Guess: ${guess}`);
 console.log(`CONGRATS YOU WIN!!`);
+
+//This is feat 9. For....Of intro
+
+let subreddits = [ 'soccer', 'popheads', 'cringe', 'books' ];
+// With a standard for loop
+for (let i = 0; i < subreddits.length; i++) {
+	console.log(subreddits[i]);
+}
+//Much cleaner  with a for...of loop!
+for (let sub of subreddits) {
+	console.log(sub);
+}
+//Works with other iterables, like strings!
+for (let char of 'cockadoodledoo') {
+	console.log(char.toUpperCase());
+}
+
+//This is feat 10. Comparing For and For...Of
+
+const magicSquare = [ [ 2, 7, 6 ], [ 9, 5, 1 ], [ 4, 3, 8 ] ];
+
+// Version using a for loop...
+for (let i = 0; i < magicSquare.length; i++) {
+	let row = magicSquare[i];
+	let sum = 0;
+	for (let j = 0; j < row.length; j++) {
+		sum += row[j];
+	}
+	console.log(`${row} summed to ${sum}`);
+}
+
+// Much cleaner version using a for...of
+for (let row of magicSquare) {
+	let sum = 0;
+	for (let num of row) {
+		sum += num;
+	}
+	console.log(`${row} summed to ${sum}`);
+}
+
+// EXAMPLE 2
+// If you need the indices, use a traditional for loop!
+const words1 = [ 'mail', 'milk', 'bath', 'black' ];
+const words2 = [ 'box', 'shake', 'tub', 'berry' ];
+
+for (let i = 0; i < words1.length; i++) {
+	//Access index i of both arrays
+	console.log(`${words1[i]}${words2[i]}`);
+}
+
+//This is feat 11. For...Of with Objects
+
+const movieReviews = {
+	Arrival                : 9.5,
+	Alien                  : 9,
+	Amelie                 : 8,
+	'In Bruges'            : 9,
+	Amadeus                : 10,
+	'Kill Bill'            : 8,
+	'Little Miss Sunshine' : 8.5,
+	Coraline               : 7.5
+};
+
+// THIS DOES NOT WORK!
+// OBJECTS ARE NOT ITERABLE (can't use a for...of loop)
+// for (let x of movieReviews) {
+// 	console.log(x);
+// }
+
+// We CAN iterate over the keys of an object
+for (let movie of Object.keys(movieReviews)) {
+	console.log(`You rated ${movie} - ${movieReviews[movie]}`);
+}
+
+// We can also iterate over the values
+// To calculate the average movie rating:
+const ratings = Object.values(movieReviews);
+let total = 0;
+for (let r of ratings) {
+	total += r;
+}
+let avg = total / ratings.length;
+console.log('Average Rating: ', avg);
+
+//This is feat 12. For...In Loops
+
+const jeopardyWinnings =  {
+    regularPlay     : 2522700,
+    watsonChallenge : 300000,
+    tournamentOfChampions: 500000,
+    battleOfTheDecades : 100000
+};
+
+for (let prop in jeopardyWinnings){
+    console.log(prop);
+    console.log(jeopardyWinnings[prop]);  
+}
+
+let total = 0;
+for (let prop in jeopardyWinnings){
+    total += jeopardyWinnings[prop];
+}

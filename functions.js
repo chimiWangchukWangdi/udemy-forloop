67718d5a091cf4f9947f34795993914e0429f579
devcww <chimiwangchukw@gmail.com>
2022-07-04 18:54:36 +0600

    feat/functions

--- functions.js
+++ functions.js
@@ -68,3 +68,217 @@ function throwDice(numRolls) {
 
 throwDice(2);
 throwDice(6);
+
+//This is 4. Multiple Arguments
+
+function square(num) {
+	console.log(num * num);
+}
+
+function sum(x, y) {
+	console.log(x + y);
+}
+
+function divide(a, b) {
+	console.log(a / b);
+}
+
+//This is 5. Return Statement
+
+// No return!
+function add(x, y) {
+	console.log(x + y);
+}
+
+// This version returns the sum of x & y;
+function add(x, y) {
+	return x + y;
+}
+
+// We can capture the return value:
+const total = add(4, 9); //13
+
+//Thid is 6. More about Return
+
+
+function square(x) {
+	return x * x;
+	console.log('ALL DONE!'); //this NEVER runs;
+}
+
+// One way of writing this function
+function isPurple(color) {
+	if (color.toLowerCase() === 'purple') {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// We don't need the else!
+function isPurple(color) {
+	if (color.toLowerCase() === 'purple') {
+		return true;
+	}
+	return false;
+}
+
+// An even shorter way!
+function isPurple(color) {
+	return color.toLowerCase() === 'purple';
+}
+
+function containsPurple(arr) {
+	for (let color of arr) {
+		if (color === 'purple' || color === 'magenta') {
+			return true;
+		}
+	}
+	return false;
+}
+
+//This is 7. Practice 1 Password Validator
+
+function isValidPassword(password, username) {
+	if (password.length < 8) {
+		return false;
+	}
+	if (password.indexOf(' ') !== -1) {
+		return false;
+	}
+	if (password.indexOf(username) !== -1) {
+		return false;
+	}
+	return true;
+}
+
+function isValidPassword(password, username) {
+	if (
+		password.length < 8 ||
+		password.indexOf(' ') !== -1 ||
+		password.indexOf(username) !== -1
+	) {
+		return false;
+	}
+	return true;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (tooShort || hasSpace || tooSimilar) return false;
+	return true;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (!tooShort && !hasSpace && !tooSimilar) return true;
+	return false;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	return !tooShort && !hasSpace && !tooSimilar;
+}
+
+//This is 8. Practice 2 Avg
+
+function avg(arr) {
+	let total = 0;
+	//loop over each num
+	for (let num of arr) {
+		//add them together
+		total += num;
+	}
+	//divide by number of nums
+	return total / arr.length;
+}
+
+//This is 9. Practice 3 Pangrams
+
+// Version using indexOf
+function isPangram(sentence) {
+	let lowerCased = sentence.toLowerCase();
+	for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+		if (lowerCased.indexOf(char) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Version using string.includes()
+// Nice and clean, but not supported in IE
+function isPangram(sentence) {
+	let lowerCased = sentence.toLowerCase();
+	for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+		if (!lowerCased.includes(char)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//This is 10. Practice 4
+
+// function getCard() {
+// 	const values = [
+// 		'1',
+// 		'2',
+// 		'3',
+// 		'4',
+// 		'5',
+// 		'6',
+// 		'7',
+// 		'8',
+// 		'9',
+// 		'10',
+// 		'J',
+// 		'Q',
+// 		'K',
+// 		'A'
+// 	];
+// 	const valIdx = Math.floor(Math.random() * values.length);
+// 	const value = values[valIdx];
+
+// 	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
+// 	const suitIdx = Math.floor(Math.random() * suits.length);
+// 	const suit = suits[suitIdx];
+// 	return { value: value, suit: suit };
+// }
+
+function pick(arr) {
+	//return random element from arr
+	const idx = Math.floor(Math.random() * arr.length);
+	return arr[idx];
+}
+
+function getCard() {
+	const values = [
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'J',
+		'Q',
+		'K',
+		'A'
+	];
+	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
+	return { value: pick(values), suit: pick(suits) };
+}
+
+
+
